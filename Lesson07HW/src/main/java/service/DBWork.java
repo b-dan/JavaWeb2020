33fa680d736330ba44463771f7c2b0bed86584c5
Пшенін Богдан Олександрович <pshenin@fuib.com>
2020-08-16 09:42:02 +0300

    lesson07HW

---
+++ Lesson07HW/src/main/java/service/DBWork.java
@@ -0,0 +1,103 @@
+package service;
+
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import model.User;
+
+public class DBWork {
+	private final String SELECT_USER = "SELECT NAME FROM `users` WHERE LOGIN = ? AND PASSWORD = ?;";
+	private final String INSERT_INTO_USERS = "INSERT INTO `users` (`ID`, `LOGIN`, `PASSWORD`, `NAME`, `REPASSWORD`, `GENDER`, `ADDRESS`, `COMMENT`, `AGREE`) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?);";
+	private final String SALT = ".dsfe4,";
+	public DBWork() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+	public Connection getConnection() {
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" + "user=root&password=");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+
+	}
+	public User getName(String login, String password) {
+		User user = null;
+		Connection con = getConnection();
+		try(PreparedStatement ps = con.prepareStatement(SELECT_USER);){
+			ps.setString(1, login);
+			ps.setString(2, hashPassword(password));
+			ResultSet rs = ps.executeQuery();
+			if(rs.next()) {
+				user = new User();
+				user.setName(rs.getString("NAME"));
+			}
+			rs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return user;
+
+	}
+	public void setUser(String login, String password, String name, String rePassword, String gender, String address, String comment, String agree) {
+		PreparedStatement ps = null;
+		Connection con = getConnection();
+		try{ps = con.prepareStatement(INSERT_INTO_USERS);
+			ps.setString(1, login);
+			ps.setString(2, hashPassword(password));
+			ps.setString(3, name);
+			ps.setString(4, hashPassword(rePassword));
+			ps.setString(5, gender);
+			ps.setString(6, address);
+			ps.setString(7, comment);
+			ps.setString(8, agree);
+			ps.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private String hashPassword(String password) throws SQLException {
+		String hashPass = "";
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			md.update(StandardCharsets.UTF_8.encode(password+SALT));
+			hashPass = String.format("%032x", new BigInteger(md.digest()));
+		} catch (NoSuchAlgorithmException e) {
+			throw new SQLException();
+		}
+		return hashPass;
+	}
+	public static void main(String[] args) {
+		DBWork dbwork = new DBWork();
+	}
+}
